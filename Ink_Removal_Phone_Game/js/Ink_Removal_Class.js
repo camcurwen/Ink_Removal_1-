ff22735376cffd52da6645b6e8e86e536bf01349
camcurwen <43749416+camcurwen@users.noreply.github.com>
2019-03-18 10:12:40 +0000

    Add files via upload

--- Ink_Removal_Phone_Game/js/Ink_Removal_Class.js
+++ Ink_Removal_Phone_Game/js/Ink_Removal_Class.js
@@ -1,17 +1,25 @@
-// Ink_Removal_Class.js
+// Ink_Removal_Scene.js
 class client {
+constructor(scene, x, y, texture, frame){
+    this.scene = scene;
+    this.ink = scene.physics.add.ink(x, y, texture, 'ink');
 
-
+  }
 }
 update(){
-
+this.ink.x = this.body.x = this.hull.x;
+    this.ink.y = this.body.y = this.hull.y;
 }
 damage(){
-
+console.log('it hurts');
 }
+//todo
 satisfaction(){
 
 }
+burn(){
+    this.body.setVisible(false);
+  }
 
 class ink {
     update() {
